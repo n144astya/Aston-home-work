47a118adedd0f2023a316a6faa0c28a8872ad058
n144asty <n144asty@gmail.com>
2024-08-01 03:05:21 +0300

    Lesson_11 fix remarks

--- src/test/java/homework/taskSixJunit/FactorialUnitTest.java
+++ src/test/java/homework/taskSixJunit/FactorialUnitTest.java
@@ -3,39 +3,34 @@ package homework.taskSixJunit;
 import org.junit.jupiter.api.Test;
 import homework.taskSix.FactorialException;
 import homework.taskSix.FactorialHelper;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FactorialUnitTest {
 
-    @Test
-    public void testFactorialOfZero() throws FactorialException {
-        assertEquals(1, new FactorialHelper().getFactorial(0));
-    }
-
-    @Test
-    public void testFactorialOfOne() throws FactorialException {
-        assertEquals(1, new FactorialHelper().getFactorial(1));
-    }
-
-    @Test
-    public void testFactorialOfTwo() throws FactorialException {
-        assertEquals(2, new FactorialHelper().getFactorial(2));
-    }
-
-    @Test
-    public void testFactorialOfFive() throws FactorialException {
-        assertEquals(120, new FactorialHelper().getFactorial(5));
-    }
-
-    @Test
-    public void testFactorialOfTen() throws FactorialException {
-        assertEquals(3628800, new FactorialHelper().getFactorial(10));
+    @ParameterizedTest
+    @CsvSource({
+            "1, 0",
+            "1, 1",
+            "121645100408832000, 19",
+            "2432902008176640000, 20"
+    })
+    public void testFactorial(long expected, int inputNumber) throws FactorialException {
+        assertEquals(expected, new FactorialHelper().getFactorial(inputNumber), "Unexpected value. Expect " + expected);
     }
 
     @Test
     public void testFactorialOfNegativeNumber() {
         FactorialException thrown = assertThrows(FactorialException.class, () -> new FactorialHelper().getFactorial(-1));
-        assertTrue(thrown.getMessage().contains("The factorial of a negative number does not exist"));
+        assertTrue(thrown.getMessage().contains("The factorial of a negative number does not exist"), "Unexpected message. Expect: 'The factorial of a negative number does not exist'");
+    }
+    @Test
+    public void testFactorialOfTooBigValue() {
+        FactorialException thrown = assertThrows(FactorialException.class, () -> new FactorialHelper().getFactorial(21));
+        assertTrue(thrown.getMessage().contains("The value is too big"), "Unexpected message. Expect: 'The value is too big'");
     }
 }
