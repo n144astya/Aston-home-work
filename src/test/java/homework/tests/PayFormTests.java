19d0a34110ad9106d3a281797b43690f989b6d23
n144asty <n144asty@gmail.com>
2024-08-10 00:34:33 +0300

    Lesson_14 fix

--- src/test/java/homework/tests/PayFormTests.java
+++ src/test/java/homework/tests/PayFormTests.java
@@ -2,10 +2,7 @@ package homework.tests;
 
 import homework.pageObjects.MtsHomePagePayForm;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,6 +17,10 @@ public class PayFormTests {
     @BeforeClass
     public static void setuo() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void before() {
         driver = new ChromeDriver();
         payForm = new MtsHomePagePayForm(driver);
 
@@ -30,29 +31,44 @@ public class PayFormTests {
     }
 
     @Test
-    public void test() {
+    public void testCheckBlockTitle() {
         payForm.discardCookie();
         payForm.scrollToPaymentForm();
         Assert.assertEquals( "Онлайн пополнение\nбез комиссии", payForm.getPaymentFormTitle());
-        Assert.assertEquals(5, payForm.getPayPartnersCount());
+    }
 
+    @Test
+    public void testPaymentsLogo() {
+        payForm.discardCookie();
+        payForm.scrollToPaymentForm();
+
+        Assert.assertEquals(5, payForm.getPayPartnersCount());
         Assert.assertEquals("Visa", payForm.getPayPartnersImgAlt(0));
         Assert.assertEquals("Verified By Visa", payForm.getPayPartnersImgAlt(1));
         Assert.assertEquals("MasterCard", payForm.getPayPartnersImgAlt(2));
         Assert.assertEquals("MasterCard Secure Code", payForm.getPayPartnersImgAlt(3));
         Assert.assertEquals("Белкарт", payForm.getPayPartnersImgAlt(4));
+    }
 
+    @Test
+    public void testCheckAboutLink() {
+        payForm.discardCookie();
+        payForm.scrollToPaymentForm();
         payForm.clickAboutLinc();
         Assert.assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", driver.getCurrentUrl());
-        driver.navigate().back();
+    }
 
+    @Test
+    public void testCheckPayments() {
+        payForm.discardCookie();
+        payForm.scrollToPaymentForm();
         payForm.completeForm("297777777", "10", "email@email.com");
         Assert.assertTrue(payForm.isPaidAppShow());
         Assert.assertTrue(payForm.isPaidAppFormContain("10", "297777777"));
     }
 
-    @AfterClass
-    public static void tearDown() {
+    @After
+    public void tearDown() {
         driver.quit();
     }
 }
