e061deb82c32c43f32495ef3ff9fe94eb3f50f2a
n144asty <n144asty@gmail.com>
2024-08-14 02:10:58 +0300

    Lesson_16 api test

--- src/test/java/homework/PostmanEchoTests.java
+++ src/test/java/homework/PostmanEchoTests.java
@@ -1,19 +1,43 @@
 package homework;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class PostmanEchoTests {
 
     @Test
-    public void testGetRequestStatusCode(){
-        given()
+    public void testGetRequest() throws IOException {
+
+        Response response = given()
                 .baseUri("https://postman-echo.com")
                 .when()
-                .get("get?foo1=bar1&foo2=bar2")
-                .then()
-                .assertThat()
-                .statusCode(200);
+                .get("get?foo1=bar1&foo2=bar2");
+        assertAll(
+                () -> response.then().assertThat().statusCode(200),
+                () -> response.then().body("args.foo1", equalTo("bar1")),
+                () -> response.then().body("args.foo2", equalTo("bar2"))
+        );
+    }
+
+    @Test
+    public void testPostRawText() {
+        String bodyText = "This is expected to be sent back as part of response body.";
+        Response response = given()
+                .baseUri("https://postman-echo.com")
+                .header("Content-Type", "text/plain")
+                .body(bodyText)
+                .when()
+                .post("post");
+
+        assertAll(
+                () -> response.then().assertThat().statusCode(200),
+                () -> response.then().body("data", equalTo(bodyText))
+        );
     }
 }
