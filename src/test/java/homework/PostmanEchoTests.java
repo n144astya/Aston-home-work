2f5b996a01123899d16f39c4be452f0eb24db62e
n144asty <n144asty@gmail.com>
2024-08-16 00:53:56 +0300

    Lesson_16 api test

--- src/test/java/homework/PostmanEchoTests.java
+++ src/test/java/homework/PostmanEchoTests.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class PostmanEchoTests {
@@ -21,7 +22,17 @@ public class PostmanEchoTests {
         assertAll(
                 () -> response.then().assertThat().statusCode(200),
                 () -> response.then().body("args.foo1", equalTo("bar1")),
-                () -> response.then().body("args.foo2", equalTo("bar2"))
+                () -> response.then().body("args.foo2", equalTo("bar2")),
+                () -> response.then().body("url", equalTo("http://postman-echo.com/get?foo1=bar1&foo2=bar2")),
+                () -> response.then().body("headers.host", equalTo("postman-echo.com")),
+                () -> response.then().body("headers.x-forwarded-proto", equalTo("http")),
+                () -> response.then().body("headers.connection", equalTo("close")),
+                () -> response.then().body("headers.x-forwarded-port", equalTo("443")),
+                () -> response.then().body("headers.accept", equalTo("*/*")),
+                () -> response.then().body("headers.accept-encoding", equalTo("gzip,deflate")),
+                () -> response.then().body("headers", hasKey("x-request-start")),
+                () -> response.then().body("headers", hasKey("x-amzn-trace-id")),
+                () -> response.then().body("headers", hasKey("user-agent"))
         );
     }
 
@@ -37,7 +48,20 @@ public class PostmanEchoTests {
 
         assertAll(
                 () -> response.then().assertThat().statusCode(200),
-                () -> response.then().body("data", equalTo(bodyText))
+                () -> response.then().body("data", equalTo(bodyText)),
+                () -> response.then().body("json", equalTo(null)),
+                () -> response.then().body("url", equalTo("http://postman-echo.com/post")),
+                () -> response.then().body("headers.host", equalTo("postman-echo.com")),
+                () -> response.then().body("headers.x-forwarded-proto", equalTo("http")),
+                () -> response.then().body("headers.connection", equalTo("close")),
+                () -> response.then().body("headers.content-length", equalTo("58")),
+                () -> response.then().body("headers.x-forwarded-port", equalTo("443")),
+                () -> response.then().body("headers.accept", equalTo("*/*")),
+                () -> response.then().body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1")),
+                () -> response.then().body("headers.accept-encoding", equalTo("gzip,deflate")),
+                () -> response.then().body("headers", hasKey("x-request-start")),
+                () -> response.then().body("headers", hasKey("x-amzn-trace-id")),
+                () -> response.then().body("headers", hasKey("user-agent"))
         );
     }
 
@@ -56,7 +80,22 @@ public class PostmanEchoTests {
                 () -> response.then().body("form.foo1", equalTo("bar1")),
                 () -> response.then().body("form.foo2", equalTo("bar2")),
                 () -> response.then().body("json.foo1", equalTo("bar1")),
-                () -> response.then().body("json.foo2", equalTo("bar2"))
+                () -> response.then().body("json.foo2", equalTo("bar2")),
+                () -> response.then().body("url", equalTo("http://postman-echo.com/post")),
+                () -> response.then().body("headers.host", equalTo("postman-echo.com")),
+                () -> response.then().body("headers.x-forwarded-proto", equalTo("http")),
+                () -> response.then().body("headers.connection", equalTo("close")),
+                () -> response.then().body("headers.content-length", equalTo("19")),
+                () -> response.then().body("headers.x-forwarded-port", equalTo("443")),
+                () -> response.then().body("headers.accept", equalTo("*/*")),
+                () -> response.then().body("headers.content-type", equalTo("application/x-www-form-urlencoded; charset=utf-8")),
+                () -> response.then().body("headers.accept-encoding", equalTo("gzip,deflate")),
+                () -> response.then().body("headers", hasKey("x-request-start")),
+                () -> response.then().body("headers", hasKey("x-amzn-trace-id")),
+                () -> response.then().body("headers", hasKey("user-agent")),
+                () -> response.then().body("$", hasKey("args")),
+                () -> response.then().body("$", hasKey("data")),
+                () -> response.then().body("$", hasKey("files"))
         );
     }
 
@@ -72,7 +111,23 @@ public class PostmanEchoTests {
 
         assertAll(
                 () -> response.then().assertThat().statusCode(200),
-                () -> response.then().body("data", equalTo(bodyText))
+                () -> response.then().body("data", equalTo(bodyText)),
+                () -> response.then().body("json", equalTo(null)),
+                () -> response.then().body("url", equalTo("http://postman-echo.com/put")),
+                () -> response.then().body("$", hasKey("args")),
+                () -> response.then().body("$", hasKey("form")),
+                () -> response.then().body("$", hasKey("files")),
+                () -> response.then().body("headers.host", equalTo("postman-echo.com")),
+                () -> response.then().body("headers.x-forwarded-proto", equalTo("http")),
+                () -> response.then().body("headers.connection", equalTo("close")),
+                () -> response.then().body("headers.content-length", equalTo("58")),
+                () -> response.then().body("headers.x-forwarded-port", equalTo("443")),
+                () -> response.then().body("headers.accept", equalTo("*/*")),
+                () -> response.then().body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1")),
+                () -> response.then().body("headers.accept-encoding", equalTo("gzip,deflate")),
+                () -> response.then().body("headers", hasKey("x-request-start")),
+                () -> response.then().body("headers", hasKey("x-amzn-trace-id")),
+                () -> response.then().body("headers", hasKey("user-agent"))
         );
     }
 
@@ -88,7 +143,23 @@ public class PostmanEchoTests {
 
         assertAll(
                 () -> response.then().assertThat().statusCode(200),
-                () -> response.then().body("data", equalTo(bodyText))
+                () -> response.then().body("data", equalTo(bodyText)),
+                () -> response.then().body("json", equalTo(null)),
+                () -> response.then().body("url", equalTo("http://postman-echo.com/patch")),
+                () -> response.then().body("$", hasKey("args")),
+                () -> response.then().body("$", hasKey("form")),
+                () -> response.then().body("$", hasKey("files")),
+                () -> response.then().body("headers.host", equalTo("postman-echo.com")),
+                () -> response.then().body("headers.x-forwarded-proto", equalTo("http")),
+                () -> response.then().body("headers.connection", equalTo("close")),
+                () -> response.then().body("headers.content-length", equalTo("58")),
+                () -> response.then().body("headers.x-forwarded-port", equalTo("443")),
+                () -> response.then().body("headers.accept", equalTo("*/*")),
+                () -> response.then().body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1")),
+                () -> response.then().body("headers.accept-encoding", equalTo("gzip,deflate")),
+                () -> response.then().body("headers", hasKey("x-request-start")),
+                () -> response.then().body("headers", hasKey("x-amzn-trace-id")),
+                () -> response.then().body("headers", hasKey("user-agent"))
         );
     }
 
@@ -104,7 +175,23 @@ public class PostmanEchoTests {
 
         assertAll(
                 () -> response.then().assertThat().statusCode(200),
-                () -> response.then().body("data", equalTo(bodyText))
+                () -> response.then().body("data", equalTo(bodyText)),
+                () -> response.then().body("json", equalTo(null)),
+                () -> response.then().body("url", equalTo("http://postman-echo.com/delete")),
+                () -> response.then().body("$", hasKey("args")),
+                () -> response.then().body("$", hasKey("form")),
+                () -> response.then().body("$", hasKey("files")),
+                () -> response.then().body("headers.host", equalTo("postman-echo.com")),
+                () -> response.then().body("headers.x-forwarded-proto", equalTo("http")),
+                () -> response.then().body("headers.connection", equalTo("close")),
+                () -> response.then().body("headers.content-length", equalTo("58")),
+                () -> response.then().body("headers.x-forwarded-port", equalTo("443")),
+                () -> response.then().body("headers.accept", equalTo("*/*")),
+                () -> response.then().body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1")),
+                () -> response.then().body("headers.accept-encoding", equalTo("gzip,deflate")),
+                () -> response.then().body("headers", hasKey("x-request-start")),
+                () -> response.then().body("headers", hasKey("x-amzn-trace-id")),
+                () -> response.then().body("headers", hasKey("user-agent"))
         );
     }
 }
